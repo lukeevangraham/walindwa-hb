5a6313640a66c690d30fa540e16da6c222f65fb9
Cefalu Wave <luke.evan.graham@gmail.com>
2020-05-18 11:28:11 -0700

    home page grabs cover image from strapi

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,9 +3,17 @@ let router = express.Router();
 let axios = require("axios");
 
 router.get("/", (req, res) => {
-  let hbsObject = {};
-  console.log(hbsObject);
-  res.render("index", hbsObject);
+  Promise.all([
+    axios.get("http://localhost:1337/home")
+  ])
+  .then(resultArray => {
+    console.log(`LOOK HERE: `, resultArray[0].data)
+    let hbsObject = {
+      homeSingle: resultArray[0]
+    };
+    // console.log(hbsObject);
+    res.render("index", hbsObject);
+  })
 });
 
 module.exports = router;
