2da542c5f255df576550988e9b50a36d48766a6d
Cefalu Wave <luke.evan.graham@gmail.com>
2020-05-27 00:21:59 -0700

    working on projects, almost done but left 'you may also like' section incomplete

--- controllers/handlebars-helpers.js
+++ controllers/handlebars-helpers.js
@@ -1,5 +1,8 @@
+let markdown = require("helper-markdown");
+
 module.exports = {
     fiveWords: function(p) {
         return p.split(" ").splice(0,5).join(" ");
-    }
+    },
+    markdown: markdown
 }
--- controllers/routes.js
+++ controllers/routes.js
@@ -73,16 +73,41 @@ router.get("/kenya", (req, res) => {
     crossorigin=""/> <!-- Make sure you put this AFTER Leaflet's CSS -->
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
       integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-      crossorigin=""></script>`
-  }
+      crossorigin=""></script>`,
+  };
   res.render("./sections/kenya/kenya", hbsObject);
-})
+});
 
 router.get("/projects", (req, res) => {
-  let hbsObject = {
-    title: "Projects"
-  }
-  res.render("./sections/kenya/projects", hbsObject)
+  Promise.all([
+    axios.get(
+      "http://admin.moreleft.com/projects?status_eq=Current&Published_eq=true&_sort=id:DESC"
+    ),
+    axios.get(
+      "http://admin.moreleft.com/projects?status_eq=Completed&Published_eq=true&_sort=id:DESC"
+    ),
+  ]).then((resultArray) => {
+    let hbsObject = {
+      title: "Projects",
+      currentProjects: resultArray[0].data,
+      completedProjects: resultArray[1].data,
+    };
+    res.render("./sections/kenya/projects", hbsObject);
+  });
+});
+
+router.get("/project:id", (req, res) => {
+  Promise.all([
+    axios.get("http://admin.moreleft.com/projects?Published_eq=true&id_eq=" + req.params.id.substr(1)),
+    axios.get("http://admin.moreleft.com/projects?Published_eq=true&id_ne=" + req.params.id.substr(1) + "&_sort=id:DESC&_limit=6")
+  ]).then((resultArray) => {
+    let hbsObject = {
+      title: resultArray[0].data[0].name,
+      project: resultArray[0].data[0],
+      otherProjects: resultArray[1].data
+    }
+    res.render("./sections/kenya/project", hbsObject)
+  })
 })
 
 module.exports = router;
