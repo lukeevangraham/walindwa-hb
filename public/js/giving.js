7060bbb1c9e5d3fa7fd91b60257c85da9001a6f2
Cefalu Wave <luke.evan.graham@gmail.com>
2020-07-24 20:59:54 -0700

    updated giving appends

--- public/js/giving.js
+++ public/js/giving.js
@@ -7,7 +7,10 @@ $(document).ready(function() {
     
 
     function checkAmt() {
-        console.log($("#cdsetdonationamt1").append(" (month sponsorship)"))
+        console.log($("#cdsetdonationamt1").append(" Half-sponsor, monthly"))
+        console.log($("#cdsetdonationamt2").append(" Full-sponsor, monthly"))
+        console.log($("#cdsetdonationamt3").append(" Half-sponsor, annually"))
+        console.log($("#cdsetdonationamt4").append(" Full-sponsor, annually"))
         
     }
 })
